14197498f0fea0f086480c7dbe50f646949ba2ff
Kenneth Torng <ktorng@gmail.com>
2016-03-24 17:54:59 -0500

    lobby and join game

--- client/main.js
+++ client/main.js
@@ -123,6 +123,11 @@ Tracker.autorun(trackGameState);
 window.onbeforeunload = resetUserState;
 window.onpagehide = resetUserState;
 
+FlashMessages.configure({
+  autoHide: true,
+  autoScroll: false
+});
+
 Template.main.helpers({
   whichView: function(){
     return Session.get('currentView');
@@ -180,6 +185,73 @@ Template.createGame.helpers({
     return Session.get('loading');
   }
 });
+
+Template.createGame.rendered = function(event) {
+  $("#player-name").focus();
+};
+
+Template.joinGame.events({
+  'submit #join-game': function(event){
+    //GAnalytics.event("game-actions", "gamejoin");
+
+    var accessCode = event.target.accessCode.value;
+    var playerName = event.target.playerName.value;
+
+    if(!playerName || Session.get('loading')) {
+      return false;
+    }
+    
+    accessCode = accessCode.trim();
+    accessCode = accessCode.toLowerCase();
+
+    Session.set('loading', true);
+
+    Meteor.subscribe('games', accessCode, function() {
+      Session.set('loading', false);
+
+      var game = Games.findOne({accessCode: accessCode});
+
+      if(game) {
+        Meteor.subscribe('players', game._id);
+        player = generateNewPlayer(game, playerName);
+
+        Session.set('urlAccessCode', null);
+        Session.set('gameID', game._id);
+        Session.set('playerID', player._id);
+        Session.set('currentView', 'lobby');
+      } else {
+        FlashMessages.sendError("invalide access code");
+        //GAnalytics.event("game-actions", "invalidcode");
+      }
+    });
+    return false;
+  },
+  'click .btn-back': function() {
+    Session.set('urlAccessCode', null);
+    Session.set('currentView', 'startMenu');
+    return false;
+  }
+});
+
+Template.joinGame.helpers({
+  isLoading: function() {
+    return Session.get('loading');
+  }
+});
+
+Template.joinGame.rendered = function(event) {
+  resetUserState();
+
+  var urlAccessCode = Session.get('urlAccessCode');
+
+  if(urlAccessCode) {
+    $("#access-code").val(urlAccessCode);
+    $("#access-code").hide();
+    $("#player-name").focus();
+  } else {
+    $("#access-code").focus();
+  }
+};
 
 Template.lobby.helpers({
   game: function() {
